63e518453a441054be58edc14965b177678aeed9
Nancy Wei <nancywei20030416@gmail.com>
2026-04-15 16:28:06 +1000

    deploy

--- target/SQL/init.sql
+++ target/SQL/init.sql
@@ -1,20 +1,23 @@
--- Create task table
-CREATE TABLE IF NOT EXISTS task (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    priority VARCHAR(20),
-    due_time TIMESTAMP,
-    completed BOOLEAN DEFAULT FALSE
-    );
-
-CREATE TABLE IF NOT EXISTS users (
-   id BIGSERIAL PRIMARY KEY,
-   username VARCHAR(100) NOT NULL,
-   email VARCHAR(150) NOT NULL UNIQUE,
-   password VARCHAR(255) NOT NULL,
-   is_active BOOLEAN DEFAULT TRUE,
-   created_at TIMESTAMP,
-   updated_at TIMESTAMP
+CREATE TABLE users (
+                       id BIGSERIAL PRIMARY KEY,
+                       username VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       password VARCHAR(255) NOT NULL,
+                       is_active BOOLEAN DEFAULT TRUE,
+                       created_at TIMESTAMP,
+                       updated_at TIMESTAMP,
+                       dark_mode BOOLEAN DEFAULT FALSE,
+                       email_notifications BOOLEAN DEFAULT TRUE,
+                       task_reminders BOOLEAN DEFAULT TRUE
 );
 
+CREATE TABLE task (
+                      id BIGSERIAL PRIMARY KEY,
+                      title VARCHAR(255) NOT NULL,
+                      description VARCHAR(255),
+                      priority VARCHAR(255),
+                      due_time TIMESTAMP,
+                      completed BOOLEAN DEFAULT FALSE,
+                      user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+                      completed_time TIMESTAMP
+);
